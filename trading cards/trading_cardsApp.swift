76f312810a5987face44380f63eca99ef1566bb5
grayden clark <gclark@lcs.on.ca>
2024-01-19 08:37:30 -0500

    Initial Commit

---
+++ trading cards/trading_cardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  trading_cardsApp.swift
+//  trading cards
+//
+//  Created by Gray Clark on 2024-01-19.
+//
+
+import SwiftUI
+
+@main
+struct trading_cardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
